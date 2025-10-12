51228ade8e73dcd035b9a03bb471bfd8bae5686e
tarun-29 <29kantiwaltarun@gmail.com>
2025-10-12 12:33:29 +0530

    add buffer clone in porfolio

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -19,6 +19,14 @@ const ProjectSection = () => {
       stack: "React, Tiptap, Tailwind",
       link: "#",
     },
+    {
+      name: "Buffer Clone",
+      description:
+        "A buffer app clone with working kanban board and event calender",
+      logo: "/buffer.png",
+      stack: "Next.js, Tailwind",
+      link: "https://buffer-clone-tarun-29s-projects.vercel.app/start",
+    },
   ];
 
   return (
