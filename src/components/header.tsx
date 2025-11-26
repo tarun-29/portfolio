c152a1ff515b12195f26f1117c2b21cb78de480f
tarun-29 <29kantiwaltarun@gmail.com>
2025-11-26 23:29:40 +0530

    fix divider and add resume link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import ThemeToggle from "./theme-toggle";
 import Link from "next/link";
+import { ExternalLinkIcon } from "lucide-react";
 
 const navLinks = [
   { name: "Home", href: "/" },
@@ -8,30 +11,47 @@ const navLinks = [
   { name: "Projects", href: "/projects" },
   { name: "Blog", href: "/blog" },
 ];
-
+const NAME = "Tarun Kantiwal";
 export const Header = () => {
   return (
     <div className="flex w-full justify-between items-center">
       <div className="flex items-center gap-3">
         <Image
           src={"/morty.png"}
-          alt="Tarun Kantiwal"
+          alt={NAME}
           width={60}
           height={60}
           className="rounded-full mt-2"
         />
         <div>
-          <div className="text-3xl">Tarun Kantiwal</div>
-          <div className="flex items-center text-sm mt-1">
+          <div className="flex text-3xl">
+            {NAME}
+            <div
+              onClick={() => {
+                window.open(
+                  "https://drive.google.com/file/d/1rH8O2fLMRowRvUdsKwshfKIEHF_D10Vk/view?usp=sharing"
+                );
+              }}
+              title={"View Full Resume"}
+              className="ml-2 mt-2 cursor-pointer"
+            >
+              {<ExternalLinkIcon size={20} />}
+            </div>
+          </div>
+          <div className="flex items-center text-sm mt-2">
             {navLinks.map((link, i) => (
               <Link key={link.name} href={link.href} legacyBehavior>
                 <a
                   className={`px-2 text-red-500 dark:text-green-500 ${
-                    i !== 0
-                      ? "border-l border-dotted border-red-500 dark:border-green-500"
-                      : ""
+                    i !== 0 ? "border-l-2 border-dashed" : ""
                   }`}
-                  style={{ borderLeftWidth: i !== 0 ? "2px" : undefined }}
+                  style={{
+                    borderLeftStyle: i !== 0 ? "dashed" : undefined,
+                    borderImage:
+                      i !== 0
+                        ? "repeating-linear-gradient(0deg, currentColor 0 4px, transparent 4px 8px) 1"
+                        : undefined,
+                  }}
                 >
                   {link.name}
                 </a>
